f521fcedf64754a1d8183ab4d76c848dd30493ad
Mike2020x <62442141+Mike2020x@users.noreply.github.com>
2023-04-21 19:52:52 -0500

    Ejercicio 2 Terminado

--- ejercicio_1.js
+++ ejercicio_1.js
@@ -1,19 +1,13 @@
+const arr = [1, 2, 3, 4, 5, 6];
 // 1.utilizar el método map de los arreglos para duplicar cada elemento
 // [2, 4, 6, 8, 10, 12]
-
-// 2.utilizar el método filter para filtrar los números pares de arr
-// [2, 4, 6]
-
-// 3.utilizar el método reduce para sumar todos los elementos
-// 21
-
-const arr = [1, 2, 3, 4, 5, 6];
-
 const doble = arr.map((element) => element * 2);
 console.log(doble);
-
+// 2.utilizar el método filter para filtrar los números pares de arr
+// [2, 4, 6]
 const pares = arr.filter((number) => number % 2 === 0);
 console.log(pares);
-
+// 3.utilizar el método reduce para sumar todos los elementos
+// 21
 const suma = arr.reduce((acc, prev) => acc + prev);
 console.log(suma);
